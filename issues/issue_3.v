fc432ec0a5a0af51078357a4493fa4bde22a9a3c
BESSON Frederic <frederic.besson@inria.fr>
2020-09-15 15:20:43 +0200

    Closes #3
    The current implementation detect that the section variables are not
    necessary for the proof.
    PS: I kind of expect the issue to re-open (with more complex dependencies).

--- issues/issue_3.v
+++ issues/issue_3.v
@@ -16,22 +16,36 @@ Section TEST.
   Qed.
 
   Lemma foo2: forall (a: nat), a + a = a * 2.
-  Proof.
+  Proof using.
     intros.
     itauto lia.
   Qed.
-
-  Ltac itauto_use_tauto ::= constr:(true).
 
   Lemma foo3: forall (a: nat), a + a = a * 2.
-  Proof.
+  Proof using o x.
     intros.
+    revert o.
     itauto lia.
   Qed.
-
 
 End TEST.
 
 About foo1. (* foo1 : forall a : nat, a + a = a * 2 *)
-About foo2. (* foo2 : forall x : nat, ok x -> forall a : nat, a + a = a * 2 *)
-About foo3. (* foo2 : forall x : nat, a + a = a * 2 *)
+About foo2. (* foo2 : forall a : nat, a + a = a * 2 *)
+About foo3. (*forall x : nat, ok x -> forall a : nat, a + a = a * 2*)
+
+Ltac same_type T1 T2 :=
+  let t1 := type of T1 in
+  let t2 := type of T2 in
+  match constr:((t1,t2)) with
+  | (?X,?X) => exact I
+  |   _     => fail
+  end.
+
+Goal True.
+  same_type foo1 foo2.
+Qed.
+
+Goal True.
+  Fail same_type foo1 foo3.
+Abort.
