1d2fce48987202e11363f7d23b24a480ffb2c169
BESSON Frederic <frederic.besson@inria.fr>
2021-03-26 17:48:25 +0100

    Increase timeout

--- benchmark/pigeon_rtauto.v
+++ benchmark/pigeon_rtauto.v
@@ -1,56 +1,64 @@
 Require Import Rtauto.
 Require Import pigeon_hole.
 
-Goal pigeon_hole false 1 0 -> False.
+ Goal pigeon_hole false 1 0 -> False.
 Proof.
   simpl_pigeon.
-  Time  timeout 1000 rtauto.
-Time Qed.
+  Time  timeout 3600 rtauto.
+  Time Qed.
+
 
 Goal pigeon_hole false 2 1 -> False.
 Proof.
   simpl_pigeon.
-Time  timeout 1000 rtauto.
+Time  timeout 3600 rtauto.
 Time Qed.
 
 Goal pigeon_hole false 3 2 -> False.
 Proof.
   simpl_pigeon.
-Time  timeout 1000 rtauto.
+Time  timeout 3600 rtauto.
 Time Qed.
 
 Goal pigeon_hole false 4 3 -> False.
 Proof.
   simpl_pigeon.
-Time   timeout 1000 rtauto.
+Time   timeout 3600 rtauto.
 Time Qed.
 
 Goal pigeon_hole false 5 4 -> False.
 Proof.
   simpl_pigeon.
-Time   timeout 1000 rtauto.
+Time   timeout 3600 rtauto.
 Time Qed.
+
 
 Goal pigeon_hole false 6 5 -> False.
 Proof.
   simpl_pigeon.
-Time   timeout 1000 rtauto.
+Time   timeout 3600 rtauto.
 Time Qed.
 
 Goal pigeon_hole false 7 6 -> False.
 Proof.
   simpl_pigeon.
-Time   timeout 1000 rtauto.
+Time   timeout 3600 rtauto.
 Time Qed.
 
 Goal pigeon_hole false 8 7 -> False.
 Proof.
   simpl_pigeon.
-Time   timeout 1000 rtauto.
+Time   timeout 3600 rtauto.
 Time Qed.
 
 Goal pigeon_hole false 9 8 -> False.
 Proof.
   simpl_pigeon.
-Time   timeout 1000 rtauto.
+Time   timeout 3600 rtauto.
+Time Qed.
+
+Goal pigeon_hole false 10 8 -> False.
+Proof.
+  simpl_pigeon.
+Time   timeout 3600 rtauto.
 Time Qed.
--- benchmark/pigeon_tauto.v
+++ benchmark/pigeon_tauto.v
@@ -3,54 +3,54 @@ Require Import pigeon_hole.
 Goal pigeon_hole false 1 0 -> False.
 Proof.
   simpl_pigeon.
-  Time  timeout 1000 tauto.
+  Time  timeout 3600 tauto.
 Time Qed.
 
 Goal pigeon_hole false 2 1 -> False.
 Proof.
   simpl_pigeon.
-Time  timeout 1000 tauto.
+Time  timeout 3600 tauto.
 Time Qed.
 
 Goal pigeon_hole false 3 2 -> False.
 Proof.
   simpl_pigeon.
-Time  timeout 1000 tauto.
+Time  timeout 3600 tauto.
 Time Qed.
 
 Goal pigeon_hole false 4 3 -> False.
 Proof.
   simpl_pigeon.
-Time   timeout 1000 tauto.
+Time   timeout 3600 tauto.
 Time Qed.
 
 Goal pigeon_hole false 5 4 -> False.
 Proof.
   simpl_pigeon.
-Time   timeout 1000 tauto.
+Time   timeout 3600 tauto.
 Time Qed.
 
 Goal pigeon_hole false 6 5 -> False.
 Proof.
   simpl_pigeon.
-Time   timeout 1000 tauto.
+Time   timeout 3600 tauto.
 Time Qed.
 
 Goal pigeon_hole false 7 6 -> False.
 Proof.
   simpl_pigeon.
-Time   timeout 1000 tauto.
+Time   timeout 3600 tauto.
 Time Qed.
 
 Goal pigeon_hole false 8 7 -> False.
 Proof.
   simpl_pigeon.
-Time   timeout 1000 tauto.
+Time   timeout 3600 tauto.
 Time Qed.
 
 Goal pigeon_hole false 9 8 -> False.
 Proof.
   simpl_pigeon.
-Time   timeout 1000 tauto.
+Time   timeout 3600 tauto.
 Time Qed.
 
